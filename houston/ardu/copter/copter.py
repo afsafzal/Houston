19cbf056b1c51c54a71f3fe02baa74172fe7be69
Afsoon Afzal <afs.afzal@gmail.com>
2018-10-18 19:07:45 -0400

    Cosmetic changes

--- houston/ardu/copter/copter.py
+++ houston/ardu/copter/copter.py
@@ -1,6 +1,7 @@
 __all__ = ['ArduCopter']
 
 import logging
+import os
 
 from .state import State
 from .sandbox import Sandbox
@@ -8,13 +9,15 @@ from .goto import GoTo
 from .setmode import SetMode
 from .takeoff import Takeoff
 from .parachute import Parachute
-from .command_factory import create_command, read_commands_yml
+from ..command_factory import read_commands_yml
 from ..base import BaseSystem
 from ..common import ArmDisarm
 from ..configuration import Configuration
 
 logger = logging.getLogger(__name__)  # type: logging.Logger
 logger.setLevel(logging.DEBUG)
+
+dirname = os.path.dirname(__file__)
 
 
 class ArduCopter(BaseSystem):
@@ -29,4 +32,4 @@ class ArduCopter(BaseSystem):
 #        SetMode,
 #        Parachute
 #        ]
-    commands = read_commands_yml()
+    commands = read_commands_yml(os.path.join(dirname, 'commands.yml'))
