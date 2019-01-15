6cacab502585024e633153be4b364a1071d698b1
Afsoon <afsoona@cs.cmu.edu>
2019-01-14 20:37:07 -0500

    changes to script5

--- script5.py
+++ script5.py
@@ -3,70 +3,7 @@ import json
 import csv
 import yaml
 
-def create_trace_file(data_dir='traces/', output_filename='ardu.dtrace'):
-    ppt_val = """
-{name}:::{sub}
-this_invocation_nonce
-{nonce}
-"""
-    var_val = """{name}
-{val}
-1
-"""
-    with open(output_filename, 'w') as f:
-        pass
-
-    for fname in os.listdir(data_dir):
-        if not (fname.endswith('json')):
-            continue
-        filename = os.path.join(data_dir, fname)
-        with open(filename, 'r') as f:
-            j = json.load(f)
-        index = 0
-        for c in j['commands']:
-            nonce='{}-{}'.format(fname, index)
-            parameters = ''
-            for name in c['command']['parameters'].keys():
-                parameters += var_val.format(name='p_{}'.format(name), val=c['command']['parameters'][name])
-            state_b = c['states'][0]
-            state_e = c['states'][-1]
-            states = ''
-            for name in state_b.keys():
-                states += var_val.format(name=name, val=state_b[name])
-            with open(output_filename, 'a') as f:
-                f.write(ppt_val.format(name=c['command']['type'], sub='ENTER', nonce=nonce))
-                f.write(parameters)
-                f.write(states)
-            states = ''
-            for name in state_e.keys():
-                states += var_val.format(name=name, val=state_e[name])
-            with open(output_filename, 'a') as f:
-                f.write(ppt_val.format(name=c['command']['type'], sub='EXIT', nonce=nonce))
-                f.write(parameters)
-                f.write(states)
-            
-            index += 1
-
-
-
-
-def create_decl_file(command_yml_filename='commands.yml', output_filename='ardu.decl'):
-    topping = """decl-version 2.0
-input-language houston
-var-comparability implicit
-"""
-    var_decl = """variable {name}
-  var-kind variable
-  dec-type {type}
-  rep-type {type}
-  flags {flags}
-  comparability 22
-"""
-    ppt_decl = """
-ppt {name}:::{sub}
-ppt-type {type}
-"""
-    state_vars = [
+state_vars = [
         {'name': 'home_latitude',
             'type': 'float',
             'flags': ''
@@ -147,8 +84,85 @@ ppt-type {type}
             'type': 'float',
             'flags': ''
             }
-        ]
+]
 
+
+def create_trace_file(data_dir='traces/', output_filename='ardu.dtrace'):
+    ppt_val = """
+{name}:::{sub}
+this_invocation_nonce
+{nonce}
+"""
+    var_val = """{name}
+{val}
+1
+"""
+    with open(output_filename, 'w') as f:
+        pass
+
+    index = 1
+    for fname in os.listdir(data_dir):
+        if not (fname.endswith('json')):
+            continue
+        filename = os.path.join(data_dir, fname)
+        with open(filename, 'r') as f:
+            j = json.load(f)
+        for c in j['commands']:
+            nonce = str(index)
+            parameters = ''
+            for name in sorted(c['command']['parameters']):
+                parameters += var_val.format(name='p_{}'.format(name), val=c['command']['parameters'][name])
+            state_b = c['states'][0]
+            state_e = c['states'][-1]
+            states = ''
+            for s in state_vars:
+                name = s['name']
+                val = state_b[name]
+                if type(val) == bool:
+                    val = int(val)
+                elif type(val) == str:
+                    val = '"{}"'.format(val)
+                states += var_val.format(name=name, val=val)
+            with open(output_filename, 'a') as f:
+                f.write(ppt_val.format(name=c['command']['type'], sub='ENTER', nonce=nonce))
+                f.write(parameters)
+                f.write(states)
+            states = ''
+            for s in state_vars:
+                name = s['name']
+                val = state_e[name]
+                if type(val) == bool:
+                    val = int(val)
+                elif type(val) == str:
+                    val = '"{}"'.format(val)
+                states += var_val.format(name=name, val=val)
+            with open(output_filename, 'a') as f:
+                f.write(ppt_val.format(name=c['command']['type'], sub='EXIT0', nonce=nonce))
+                f.write(parameters)
+                f.write(states)
+            
+            index += 1
+
+
+
+
+def create_decl_file(command_yml_filename='commands.yml', output_filename='ardu.decls'):
+    topping = """decl-version 2.0
+input-language houston
+var-comparability implicit
+"""
+    var_decl = """variable {name}
+  var-kind variable
+  dec-type {type}
+  rep-type {type}
+  flags not_ordered {flags}
+  comparability 22
+"""
+    ppt_decl = """
+ppt {name}:::{sub}
+ppt-type {type}
+"""
+    
     states = ''
     for s in state_vars:
         states += var_decl.format(**s)
@@ -159,7 +173,7 @@ ppt-type {type}
         f.write(topping)
 
     for c in all_commands:
-        parameters = ''
+        params = []
         for i in range(1, 8):
             p = 'p{}'.format(i)
             if p in c:
@@ -176,17 +190,20 @@ ppt-type {type}
                             'type': 'float',
                             'flags': 'nomod'
                             }
-                parameters += var_decl.format(**p_dic)
+                params.append(p_dic)
+        params = sorted(params, key=lambda a: a['name'])
+        params = [var_decl.format(**p) for p in params]
+        parameters = ''.join(params)
         with open(output_filename, 'a') as f:
             f.write(ppt_decl.format(name='factory.{}'.format(c['name']), sub='ENTER', type='enter'))
             f.write(parameters)
             f.write(states)
             f.write('\n')
-            f.write(ppt_decl.format(name='factory.{}'.format(c['name']), sub='EXIT', type='exit'))
+            f.write(ppt_decl.format(name='factory.{}'.format(c['name']), sub='EXIT0', type='exit'))
             f.write(parameters)
             f.write(states)
 
 
 if __name__=="__main__":
-#    create_decl_file()
+    create_decl_file()
     create_trace_file()
