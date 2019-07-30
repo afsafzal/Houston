dc843eba5f3cbf64a3b422262a6bb1669e84f5ad
Afsoon Afzal <afs.afzal@gmail.com>
2019-07-30 18:33:43 -0400

    Adding files for template-based missions

---
+++ experiments/generate_missions_templatebased.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+import json
+import logging
+import argparse
+import random
+
+from houston.mission import Mission
+from houston.generator.template_based import TemplateBasedMissionGenerator
+from houston.generator.resources import ResourceLimits
+
+from settings import sut, initial, environment, build_config
+
+
+def setup_logging() -> None:
+    log_to_stdout = logging.StreamHandler()
+    log_to_stdout.setLevel(logging.DEBUG)
+    logging.getLogger('houston').addHandler(log_to_stdout)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Generates missions')
+    parser.add_argument('number_of_mission', type=int, action='store',
+                       help='number of missions to be generated.')
+    parser.add_argument('max_num_commands', type=int, action='store',
+                        help='maximum number of commands in a single mission.')
+    parser.add_argument('--speedup', action='store', type=int,
+                        default=1,
+                        help='simulation speedup that should be used')
+    parser.add_argument('--seed', action='store', type=int,
+                        default=1000,
+                        help='random seed to be used by random generator.')
+    parser.add_argument('--output', action='store', type=str,
+                        default='missions.json',
+                        help='the file where the results will be stored')
+    return parser.parse_args()
+
+
+def generate(num_missions: int,
+             max_num_commands: int,
+             seed: int,
+             output_file: str,
+             speedup: int
+             ) -> None:
+    random.seed(seed)
+    config = build_config(speedup)
+    mission_generator = TemplateBasedMissionGenerator(sut, initial, environment, config, max_num_commands=max_num_commands)
+    resource_limits = ResourceLimits(num_missions)
+    missions = mission_generator.generate(seed, resource_limits,
+                template='TAKEOFF(alt:5.2)-.^*-LAND^2-.^1')
+    with open(output_file, "w") as f:
+        mission_descriptions = list(map(Mission.to_dict, missions))
+        json.dump(mission_descriptions, f, indent=2)
+
+
+if __name__ == "__main__":
+    setup_logging()
+    args = parse_args()
+    generate(num_missions=args.number_of_mission,
+             max_num_commands=args.max_num_commands,
+             seed=args.seed,
+             output_file=args.output,
+             speedup=args.speedup)
---
+++ houston/generator/template_based.py
@@ -0,0 +1,120 @@
+from typing import Type, Dict, Callable, Optional, Any
+import logging
+import attr
+import re
+
+from .base import MissionGenerator
+from ..mission import Mission
+from ..system import System
+from ..state import State
+from ..environment import Environment
+from ..configuration import Configuration
+from ..command import Command
+
+logger = logging.getLogger(__name__)   # type: logging.Logger
+logger.setLevel(logging.DEBUG)
+
+
+@attr.s
+class CommandTemplate:
+    cmd = attr.ib(type=str)
+    repeats = attr.ib(type=int)
+    params = attr.ib(type=Optional[Dict[str, Any]], default=None)
+
+    @staticmethod
+    def from_str(template_str: str) -> 'CommandTemplate':
+        regex = "(?P<cmd>[a-zA-Z\.]+)(?P<params>\(.*\))?(?P<repeats>\^[\d\*]*)?"
+        matched = re.fullmatch(regex, template_str.strip())
+        if not matched:
+            logger.error("Template is wrong %s", template_str)
+            return None
+        groups = matched.groupdict()
+        cmd = groups['cmd']
+        params = None
+        repeats = 1
+        if groups['params']:
+            pairs = groups['params'].strip()[1:-1].split(",")
+            params = {}
+            for pair in pairs:
+                splited = pair.split(":")
+                assert len(splited) == 2
+                params[splited[0].strip()] = eval(splited[1])
+        if groups['repeats']:
+            r = groups['repeats'][1:].strip()
+            if r == '*':
+                repeats = -1
+            else:
+                repeats = int(r)
+        return CommandTemplate(cmd, repeats, params)
+
+    def __str__(self):
+        s = "cmd: {}, params:{}, repeats: {}"
+        return s.format(self.cmd,
+                        self.params,
+                        self.repeats)
+
+
+class TemplateBasedMissionGenerator(MissionGenerator):
+    def __init__(self,
+                 system: Type[System],
+                 initial_state: State,
+                 env: Environment,
+                 config: Configuration,
+                 threads: int = 1,
+                 command_generators: Optional[Dict[str, Callable]] = None,
+                 max_num_commands: int = 10
+                 ) -> None:
+        super().__init__(system, threads, command_generators, max_num_commands)
+        self.__initial_state = initial_state
+        self.__env = env
+        self.__configuration = config
+
+    @property
+    def initial_state(self):
+        """
+        The initial state used by all missions produced by this generator.
+        """
+        return self.__initial_state
+
+    @property
+    def env(self):
+        """
+        The environment used by all missions produced by this generator.
+        """
+        return self.__env
+
+    def generate_command(self, command_class: Type[Command]) -> Command:
+        generator = self.command_generator(command_class)
+        if generator is None:
+            return command_class.generate(self.rng)
+        # g = generator.generate_action_without_state
+        # return g(self.system, self.__env, self.rng)
+        return g(self.rng)
+
+    def generate_mission(self, template: str):
+        command_templates = [CommandTemplate.from_str(t) \
+            for t in template.split("-")]
+        logger.info("COMMANDS: %s", command_templates)
+        raise Exception
+        command_classes = list(self.system.commands.values())
+        takeoff = None
+        for c in command_classes:
+            if "MAV_CMD_NAV_TAKEOFF" in c.uid:
+                takeoff = c
+                break
+        if not takeoff:
+            raise Exception("No TAKEOFF command found")
+        commands = [self.generate_command(takeoff)]
+        cmds_len = self.max_num_commands
+        for i in range(1, cmds_len):
+            next_allowed = commands[i - 1].__class__.get_next_allowed(self.system)  # noqa: pycodestyle
+            if next_allowed:
+                command_class = self.rng.choice(next_allowed)
+                commands.append(self.generate_command(command_class))
+            else:
+                break
+        return Mission(self.__configuration,
+                       self.__env,
+                       self.__initial_state,
+                       commands,
+                       self.system)
